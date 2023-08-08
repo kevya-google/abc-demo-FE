e01e59136ae702552c602d60452f0f38b9810e96
Kevin Yang <kevya@google.com>
2023-08-08 21:12:45 +0000

    no format save

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react text', () => {
